ad4602cd85bcf042d39ae990b2273c4808f8008f
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-04-22 11:57:18 -0400

    test() Implementando testes do requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,20 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1 - Iniciando projeto
 def test_ingredient():
-    pass
+    banana_ingredient = Ingredient("banana")
+    cereja_ingredient = Ingredient("cereja")
+    bacon_ingredient = Ingredient("bacon")
+    bacon_ingredient2 = Ingredient("bacon")
+
+    assert banana_ingredient != cereja_ingredient
+    assert bacon_ingredient == bacon_ingredient2
+
+    assert banana_ingredient.name == "banana"
+
+    assert hash(banana_ingredient) != hash(cereja_ingredient)
+    assert hash(bacon_ingredient) == hash(bacon_ingredient2)
+
+    assert repr(banana_ingredient) != "Ingredient('cereja')"
+    assert repr(banana_ingredient) == "Ingredient('banana')"
+
+    assert len(bacon_ingredient.restrictions) == 2
